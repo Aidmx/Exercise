aa1ca8c973b29989decf67889f86675dda54b113
allen <allen_lei@yeah.net>
2019-07-04 23:27:21 +0800

    feat: 四则运算的后缀表达式计算

--- 3/3_22.h
+++ 3/3_22.h
@@ -1,28 +1,89 @@
 #pragma once
 
+#include<iostream>
 #include<stack>
-
+#include<vector>
+#include<string>
+#include <sstream>
 
 class PostfixExpression
 {
-	double Calculate(char* pExpression[], int arg)
+public:
+	double mResult;
+
+
+public:
+	PostfixExpression() :mResult{0}
+	{
+	}
+
+	void DoCalculate(std::string pExpression)
 	{
 		std::stack<double> stac;
 		double result, a, b;
-		for (int i = 0; i < arg; i++)
+		std::string num;
+		for (int k = 0; k < pExpression.length(); k++)
 		{
-			result = atof(pExpression[i]);
-
-			if (result)
+			switch (pExpression[k])
 			{
-				
+			case '+':
+			{
+				a = stac.top();
+				stac.pop();
+				b = stac.top();
+				stac.pop();
+				stac.push(a + b);
 			}
-
-
-
+			break;
+			case '-':
+			{
+				a = stac.top();
+				stac.pop();
+				b = stac.top();
+				stac.pop();
+				stac.push(a - b);
+			}
+			break;
+			case '*':
+			{
+				a = stac.top();
+				stac.pop();
+				b = stac.top();
+				stac.pop();
+				stac.push(a * b);
+			}
+			break;
+			case '/':
+			{
+				//³öÕ» 
+				a = stac.top();
+				stac.pop();
+				b = stac.top();
+				stac.pop();
+				stac.push(a / b);
+			}
+			break;
+			case '=':
+			{
+				mResult = stac.top();
+				return;
+			}
+			case ' ':
+			{
+				if (num.length() > 0)
+				{
+					std::stringstream strm(num);
+					strm >> result;
+					stac.push(result);
+					num = "";
+				}
+			}
+			break;
+			default:
+				num += pExpression[k];
+				break;
+			}
 		}
-
-		return 0.0f;
 	}
 };
 
