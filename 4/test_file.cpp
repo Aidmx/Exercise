9474ce22862212b18e99d495139e1449bd732323
wanglei <wanglei@sheencity.com>
2019-07-12 21:00:18 +0800

    chore: 暂存

--- 4/test_file.cpp
+++ 4/test_file.cpp
@@ -1,15 +1,3 @@
-#ifndef _4_50_H_
-#define _4_50_H_
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstring>
-
-#define MAX_BUFFER 1024
-
-#define STEP " "
-
 void load_cpp_file(const std::string &filepath)
 {
     //按照行 load file
@@ -18,20 +6,16 @@ void load_cpp_file(const std::string &filepath)
     file.open(filepath);
     std::string line;
     char buff[MAX_BUFFER + 1];
-    bool jump = false;
-    int linenum = -1;
     while (getline(file,line))
-    {   
-        linenum++;
+    {
         memset(buff, 0 ,MAX_BUFFER + 1);
         memcpy(buff,line.c_str(),MAX_BUFFER);
-        char* token  = std::strtok(buff, STEP);
+        char *token  = std::strtok(buff, STEP);
         while (token != nullptr)
         {
-            std::cout << token ;
+            std::cout << token <<std::endl;
             token  = std::strtok(NULL, STEP);
         }
-        std::cout << std::endl ;
     }
 
     // 排序 按照字母顺序
@@ -39,5 +23,3 @@ void load_cpp_file(const std::string &filepath)
     std::cout << "end" << std::endl;
     // 输出
 }
-
-#endif
