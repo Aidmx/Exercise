d0c5cade1ff4a5f573aa2bfe0ab792d05f93b8d0
allen <allen_lei@yeah.net>
2020-05-23 20:16:16 +0800

    fix : 修改策略模式的 描述

--- design_patterns/main.cpp
+++ design_patterns/main.cpp
@@ -4,7 +4,8 @@
 #include "strategy/context.h"
 #include "strategy/strategy.h"
 #include "strategy/car_strategy.h"
-
+#include "template_method/fuel_up_car.h"
+#include "template_method/fuel_up_audi_car.h"
 
 #ifndef SAFE_DELETE
 #define SAFE_DELETE(p) { if(p){delete(p); (p)=NULL;} }
@@ -12,9 +13,23 @@
 
 int main(int argc, char* argv[])
 {
+    /* 
+        strategy 策略模式
+    */
     IStrategy* strategy = (new CarStrategy()); 
     Context context(strategy);
     context.Travel();
+    SAFE_DELETE(strategy);
 
+    /* 
+        template Method 模板方法
+    */
+    IFuelUpCar* fuelup = new FuelUpAudiCar();
+    fuelup->StopCar();
+    fuelup->OpenFuelFiller();
+    fuelup->FuelUp();
+    
+    SAFE_DELETE(fuelup);
+    system("pause");
     return 0;
 }
